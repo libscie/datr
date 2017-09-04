0aedba41aa67ca37051eea519b807347468ec88a
CHJ Hartgerink <chjh@protonmail.com>
2017-09-04 11:52:13 +0200

    Update install function

--- R/advanced.R
+++ R/advanced.R
@@ -2,9 +2,8 @@
 
 install_dat <- function (link, dir = link, ...) {
   clone_dat(link, dir, ...)
-  setwd(dir)
-  built <- devtools::build(binary = TRUE, args = c('--preclean'))
-  install.packages(built, repos = NULL, type = "source")
 
-  system(sprintf('cd ../; rm -rf %s %s', dir, built))
+  devtools::install(dir)
+
+  system(sprintf('cd ../; rm -rf %s', dir))
 }

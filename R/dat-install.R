46b5493c20f874296ab00373898de04b08b5ee75
CHJ Hartgerink <chjh@protonmail.com>
2017-11-08 23:08:24 +0100

    Add install_dat()
    With this function it is possible to directly install R packages from a Dat link.

---
+++ R/dat-install.R
@@ -0,0 +1,23 @@
+#' Install R package from Dat link
+#'
+#' Directly install an R package from a Dat link.
+#'
+#' @param link Dat link
+#'
+#' @return Message.
+#' @export
+#'
+#' @examples \dontrun{
+#'   install_dat('dat://datr-chris.hashbase.io')
+#' }
+
+install_dat <- function (link) {
+  dir <- 'tmp'
+  dat_clone(link, dir)
+
+  devtools::install(dir)
+
+  unlink(dir, recursive = TRUE)
+
+  message('Successfully installed package from Dat network')
+}

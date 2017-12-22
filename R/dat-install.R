31ed090f018b7cea2818f9fde56875b9c53a9e10
Chris Hartgerink <chjh@protonmail.com>
2017-12-22 15:54:15 +0100

    Functions to install Dat + node

--- R/dat-install.R
+++ R/dat-install.R
@@ -22,8 +22,20 @@ dat_install <- function (link) {
   message('Successfully installed package from Dat network')
 }
 
-# Install Dat using npm
+#' Install Dat from npm
+#' 
+#' Directly install Dat from npm. If node + npm are not yet available, the 
+#' function will first try to install these. NOTE: at the moment the
+#' install_mac() and install_windows() functions have not been tested (TBD).
+#' 
+#' @param os 'windows', 'mac', or 'linux'
+#' @param pkg Package manager (only for linux; typically 'apt' or 'dnf')
+#' 
+#' @return NULL. Prints stdout of terminal along the way.
+#' @export
 
-install_dat <- function () {
+install_dat <- function (os = 'windows', pkg = 'apt') {
+  tryCatch(system('npm -v'), 
+    finally = install_node(os = os, pkg = pkg))
   system('npm install -g dat')
 }
--- R/utils-dat.R
+++ R/utils-dat.R
@@ -90,3 +90,88 @@ verify_pipeline <- function (x) {
   verify_protocol(x)
   verify_hash(x)
 }
+
+#' Install node with brew on Mac
+#' 
+#' In order to install Dat, npm needs to be available on the system call. 
+#' This becomes available when nodeJS (\url{https://nodejs.org}) is available.
+#' Using Brew (\url{https://brew.sh/}), this function installs node. 
+#' 
+#' The function wraps a system call that gets an install script and uses Ruby
+#' to install it. 
+#' 
+#' @return Nothing. Prints terminal output.
+#' @export
+
+install_mac <- function () {
+  cmd <- '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+
+  tryCatch(system('brew'),
+    finally = system(cmd))
+  system('brew node')
+}
+
+#' Install node with Chocolatey on windows
+#' 
+#' In order to install Dat, npm needs to be available on the system call. 
+#' This becomes available when nodeJS (\url{https://nodejs.org}) is available.
+#' Using Chocolatey (\url{https://chocolatey.org/}), this function installs 
+#' node. 
+#' 
+#' The function wraps a system call that gets the install script from
+#' Chocolatey.
+#' 
+#' @return Nothing. Prints terminal output.
+#' @export
+
+install_windows <- function () {
+  cmd <- '@"%SystemRoot%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\\chocolatey\\bin"'
+
+  tryCatch(system('choco'), 
+    finally = system(cmd))
+  system('choco install nodejs')
+}
+
+#' Install node on Linux
+#' 
+#' In order to install Dat, npm needs to be available on the system call. 
+#' This becomes available when nodeJS (\url{https://nodejs.org}) is available.
+#' Using the specified package manager (usually \code{apt} [Debian based] or
+#' \code{dnf} [Redhat based]), this function installs node. 
+#' 
+#' In order to ensure sudo is not needed to install with \code{npm} in 
+#' \code{install_dat()}, the NODE_PATH is exported into the user's .profile
+#' 
+#' @param pkg Package manager of Linux distro (typically \code{apt} or 
+#'   \code{dnf})
+#' 
+#' @return Nothing. Prints terminal output.
+#' @export
+
+install_linux <- function (pkg = 'apt') {
+  cmd <- sprintf('sudo %s install nodejs', pkg)
+  system(cmd)
+  system('echo export NODE_PATH=~/.npm-global/lib/node_modules/ >> .profile')
+}
+
+#' Wrap node install functions across OS
+#' 
+#' @param os 'windows', 'mac', or 'linux'
+#' @param pkg Package manager [only for 'linux']
+#' 
+#' @return Nothing. Prints terminal output.
+
+install_node <- function(os, pkg) {
+  if (os == 'mac') {
+    install_mac()
+  } else if (os == 'windows') {
+    install_windows()
+  } else if (os == 'linux') {
+    install_linux(pkg = pkg)
+  } else {
+    stop('Unexpected error. Please post an issue to github.com/libscie/datr')
+  }
+}
+
+# To do
+update_npm <- function () {}
